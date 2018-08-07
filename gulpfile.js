91431afe19b65fec3a8a3bf2c0c42c39c292518d
Meletis <melnikolaidis86@gmail.com>
2018-08-08 00:09:37 +0300

    Added gulp and laravel elixir automation

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var elixir = require('laravel-elixir');
+elixir.config.sourcemaps = false;
+
+var gulp = require('gulp');
+
+elixir(function (mix) {
+    //compile sass to css
+    mix.sass('resources/assets/sass/app.scss', 'resources/assets/css');
+
+    //combine css file
+    mix.styles(
+        [
+            'css/app.css',
+            'bower/vendor/slick-carousel/slick/slick.css'
+
+        ], 'public/css/all.css', //output file
+        'resources/assets');
+
+    var bowerPath = 'bower/vendor';
+
+    mix.scripts(
+        [
+            //Jquery
+            bowerPath + '/jquery/dist/jquery.min.js',
+
+            //Foundation js
+            bowerPath + '/foundation-sites/dist/js/foundation.min.js',
+
+            //Other dependencies
+            bowerPath + '/slick-carousel/slick/slick.min.js'
+        ], 'public/js/all.js', 'resources/assets');
+});
+
